2d0701ed861e65298af105a16a502f4bc2d38230
UtkarshPrakash <48208181+UtkarshPrakash@users.noreply.github.com>
2025-02-02 15:11:46 +0530

    Add files via upload

---
+++ quick-commands.sh
@@ -0,0 +1,8 @@
+## Changing Text Scale Factor 
+## (Effectively changing the "Large Text" in Settings > Accessibility
+## Change to 1.25 for 125% Fractional Scaling
+
+gsettings set org.gnome.desktop.interface text-scaling 1.0
+
+
+
